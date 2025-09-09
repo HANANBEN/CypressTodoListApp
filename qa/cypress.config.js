3a45069968335b8f95d527ff4f4f94e3683b35b3
HANANBEN <hbenazize@gmail.com>
2025-09-09 06:04:31 +0200

    mochaawseme reports + video/screenshots reports

---
+++ qa/cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+
+
+module.exports = defineConfig({
+reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "qa/cypress/reports/mocha_reports",
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+  screenshotsFolder: "qa/cypress/reports/screenshots", 
+  videosFolder: "qa/cypress/reports/videos",           
+  video: true, 
+  screenshotOnRunFailure: true,
+ 
+                                     
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "qa/cypress/e2e/spec_definitions/**/*.cy.js",
+    supportFile: "qa/cypress/support/e2e.js",
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+
+    },
+  },
+});
